38490f2b30a1a81d054163633ccfe95f6002c208
maryaaamm <maryamfirdaus2001@gmail.com>
2024-04-24 12:08:40 +0800

    code update

--- test.js
+++ test.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/trial', (req, res) => {
+  res.send('turtle')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})``
+})
